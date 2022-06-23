b25da8519cb09143cea6da3d167f2a0e1c0c8343
HonzaSiska <siskajan@hotmail.com>
2022-06-23 20:45:49 +0200

    db connection, register and login in the db + jwt generation

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log('server')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000 
+const app = express()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const colors = require('colors')
+// const { connect } = require('mongoose')
+const connectDB = require('./config/db')
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//CONNECT TO THE DB
+connectDB()
+
+
+app.get('/', ( req, res ) => {
+    res.status(200).json({"message" : "hello from server"})
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+// middleware
+app.use(errorHandler)  
+
+
+app.listen(PORT, ()=> console.log(`Server  is running on port ${PORT}`)
+)
